e5b7ece2b8d3eb69f73bbbe03c8491d2e972d0c7
Ambujse <blastedaway94.com>
2025-11-19 06:23:44 +0000

    B: extracting Dates

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import csv
-form datetime import datetime
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -10,25 +10,34 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, header_row)
-
-# Extract high Temperatures.
-highs = []
+# Extract dates and high Temperatures.
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
-print(highs)
+# for index, column_header in enumerate(header_row):
+#     print(index, header_row)
+
+# # Extract high Temperatures.
+# highs = []
+# for row in reader:
+#     high = int(row[4])
+#     highs.append(high)
+
+# print(highs)
 
 # Plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
 
 # Format Plot
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('Temperature(F)', fontsize = 16)
 ax.tick_params(labelsize=16)
 
