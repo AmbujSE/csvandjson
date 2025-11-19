a5d5622627d5147a007a9a4a78ee0cad086cfdee
Ambujse <blastedaway94.com>
2025-11-19 06:31:36 +0000

    C: Plotting Dates

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -11,12 +11,14 @@ reader = csv.reader(lines)
 header_row = next(reader)
 
 # Extract dates and high Temperatures.
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # for index, column_header in enumerate(header_row):
 #     print(index, header_row)
@@ -29,13 +31,14 @@ for row in reader:
 
 # print(highs)
 
-# Plot the high temperatures.
+# Plot the high and low temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # Format Plot
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and low Temperatures, 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature(F)', fontsize = 16)
