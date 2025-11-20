d2b6f2db934ebc526c3a0f121f7b458de73d2b1a
Ambujse <blastedaway94.com>
2025-11-20 06:30:00 +0000

    D: plotting death valley

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,35 +1,28 @@
 from pathlib import Path
 import csv
 from datetime import datetime
-
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/sitka_weather_2021_simple.csv')
+
+path = Path('weather_data/death_valley_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Extract dates and high Temperatures.
+#Extract dates, and high and low temperatures.
 dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[4])
-    low = int(row[5])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
-
-# for index, column_header in enumerate(header_row):
-#     print(index, header_row)
-
-# # Extract high Temperatures.
-# highs = []
-# for row in reader:
-#     high = int(row[4])
-#     highs.append(high)
-
-# print(highs)
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 # Plot the high and low temperatures.
 plt.style.use('seaborn')
@@ -38,11 +31,14 @@ ax.plot(dates, highs, color='red', alpha=0.5)
 ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-# Format Plot
-ax.set_title("Daily High and low Temperatures, 2021", fontsize=24)
+# Format plot.
+title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature(F)', fontsize = 16)
 ax.tick_params(labelsize=16)
 
 plt.show()
+# for index, column_header in enumerate(header_row):
+#     print(index, column_header)
