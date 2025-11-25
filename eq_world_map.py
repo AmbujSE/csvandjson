bc668c1af2a1d9fa8b81c00a3fb38ed12034aca2
Ambujse <blastedaway94.com>
2025-11-25 10:44:58 +0000

    G: Ploting on the world map

--- eq_world_map.py
+++ eq_world_map.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import json
+import plotly.express as px
 
-# Read data as a string and convert to a Python object.
 path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
@@ -24,8 +24,6 @@ print(lons[:5])
 print(lats[:5])
 print(len(all_eq_dicts))
 
-# Create a more readable version of the data file.
-path = Path('eq_data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
-
+title = 'Global Earthquakes'
+fig = px.scatter_geo(lat=lats, lon= lons, title= title)
+fig.show()
